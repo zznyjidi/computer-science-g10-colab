22b91e7345a1028dd4bff407d5acb948d4f0775d
ma <maqianyi916@163.com>
2024-03-19 11:35:17 -0400

    add

---
+++ main.py
@@ -0,0 +1,7 @@
+#Group member: Qianyi, Zixuan, Chengyi, Tommy
+#Date: Mar. 19, 2024
+#Purpose: Create a program about the sky
+
+season = input("Season: ")
+time_hour, time_minute = map(int, input("Enter the time (Hour[space]Minute): ").split())
+weather = input("Weather (sunny, cloudy, rainy, snowy):")
