314ff64e06b8a5c8bf98a70e753dc8cd9fd8edac
ma <maqianyi916@163.com>
2024-03-19 16:09:11 -0400

    boolean

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
 #Group member: Qianyi, Zixuan, Chengyi, Tommy
 #Date: Mar. 19, 2024
-#Purpose: Create a program about the sky
+#Purpose: Mystery Madness Code 2 - Doc 1 - Create a program about the sky
 
-weather = 
+#weather = 
 
 def main():
-    global weather
-    season, time_hour = inp()
+    #global weather
+    season, time_hour, weather = inp()
     if season == "spring":
         if time_hour >= 19 or time_hour <= 7:
+            istime = True
             if weather == "sunny":
-                yes()
+                isweather = True
             else:
-                no_weather()
+                isweather = False
         else:
-            no_time()
+            istime = False
     elif season == "summer":
         if time_hour >= 20 or time_hour <= 5:
+            istime = True
             if weather == "sunny":
-                yes()
+                isweather = True
             else:
-                no_weather()
+                isweather = False
         else:
-            no_time()
+            istime = False
     elif season == "autumn" or season == "fall":
+        istime = True
         if time_hour >= 19 or time_hour <= 7:
             if weather == "sunny":
-                yes()
+                isweather = True
             else:
-                no_weather()
+                isweather = False
         else:
-            no_time()
+            istime = False
     elif season == "winter":
+        istime = True
         if time_hour >= 17 or time_hour <= 8:
             if weather == "sunny":
-                yes()
+                isweather = True
             else:
-                no_weather()
+                isweather = False
         else:
-            no_time()
+            istime = False
     else:
         print("N/A")
+
+    if istime and isweather:
+        yes()
+    elif not istime:
+        no_time()
+    else:
+        no_weather()
 
 def inp():
     season = input("Season (lowercase): ")
     time_hour, time_minute = map(int, input("Enter the time (Hours[space]Minute)(24hours): ").split())
-    return season, time_hour
+    weather = input("Weather: ")
+    return season, time_hour, weather
 
 def no_weather():
     print("There are no visible stars in the sky...\nFind a better weather")
