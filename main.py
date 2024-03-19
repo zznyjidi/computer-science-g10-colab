08e27c24ebf659b6780cd8735284211863da7d1c
ma <maqianyi916@163.com>
2024-03-19 11:43:58 -0400

    1

--- main.py
+++ main.py
@@ -5,10 +5,13 @@
 def main():
     season, time_hour, time_minute, weather = inp()
     if season == "spring":
-        
+        if 7 <= time_hour <= 19:
+            if weather == "sunny":
+                print("There are visible stars in the sky! ")
+                
 
 def inp():
     season = input("Season (lowercase): ")
-    time_hour, time_minute = map(int, input("Enter the time (Hour[space]Minute): ").split())
+    time_hour, time_minute = map(int, input("Enter the time (Hour[space]Minute)(24hours): ").split())
     weather = input("Weather (sunny, cloudy, rainy, snowy):")
     return season, time_hour, time_minute, weather
