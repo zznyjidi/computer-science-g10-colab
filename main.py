ffc7292f617937d542f0438720a0470c893f0854
ma <maqianyi916@163.com>
2024-03-19 11:41:59 -0400

    1

--- main.py
+++ main.py
@@ -3,10 +3,12 @@
 #Purpose: Create a program about the sky
 
 def main():
-    
+    season, time_hour, time_minute, weather = inp()
+    if season == "spring":
+        
 
 def inp():
-    season = input("Season: ")
+    season = input("Season (lowercase): ")
     time_hour, time_minute = map(int, input("Enter the time (Hour[space]Minute): ").split())
     weather = input("Weather (sunny, cloudy, rainy, snowy):")
     return season, time_hour, time_minute, weather
