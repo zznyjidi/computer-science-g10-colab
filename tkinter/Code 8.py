a152d2e51eee5ab55a5778fd2cb16563c71927f9
ma <maqianyi916@163.com>
2024-04-29 11:38:05 -0400

    1

--- tkinter/Code 8.py
+++ tkinter/Code 8.py
@@ -14,6 +14,13 @@ def day():
     Moon = False
     return True
 
+instruction = Label(w, text='''Welcome User428, this is the CIA's secret"\
+    operation. Operation SKYFALL.
+    During 1898, we have replaced birds with drones,"\
+    and now in the past 5 years we have"\
+    been working on deleting the sky ")
+    Lets see how loyal you are to our cause…''')
+
 l1 = Label(w, text="Is it Day or Night"\
                          "real? (Enter \"day\" or \"night\")")
 buttonDay = Button(w, text='Day', command=day)
