2e771e76ac12a04eacfdd8f37b4264fe21f1fc10
ma <maqianyi916@163.com>
2024-04-29 11:32:30 -0400

    1

--- tkinter/Code 8.py
+++ tkinter/Code 8.py
@@ -1,8 +1,21 @@
 from tk_scene import scene
+from tkinter import *
 
 sceneLayout = (
     (())
 )
 
 w = scene()
+
+def day():
+    global Sun
+    global Moon
+    Sun = True
+    Moon = False
+    return True
+
+l1 = Label(w, text="Is it Day or Night"\
+                         "real? (Enter \"day\" or \"night\")")
+buttonDay = Button(w, text='Day', command=day)
+
 
